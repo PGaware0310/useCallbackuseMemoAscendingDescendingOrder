e77eaeb44ef736052ecb92b910fa8e370d32abd9
PGaware0310 <pooja.gaware0310>
2023-06-05 22:48:41 +0530

    first_commit

--- localBooking.js
+++ localBooking.js
@@ -13,72 +13,81 @@ function saveTo(event) {
         phn
     }
 
-//JSON converts JS obj to string(Exchange data to/from server)
-// localStorage.setItem(obj.email, JSON.stringify(obj));
-// showUserDetails(obj);
+    //JSON converts JS obj to string(Exchange data to/from server)
+    // localStorage.setItem(obj.email, JSON.stringify(obj));
+    // showUserDetails(obj);
 
-axios.post("https://crudcrud.com/api/58e99c6e52c74033818ad6c2b29a8f7c/bookingApp",obj)
-.then((response)=>{
-    showUserDetails(response.data)
-    console.log(response)
-})
-.catch((err)=>console.log(err))
-
-window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/58e99c6e52c74033818ad6c2b29a8f7c/bookingApp")
+    axios.post("https://crudcrud.com/api/0a5e5fbcaa49470e9a9394cc1aa41e59/Bookingapp", obj)
         .then((response) => {
-            // console.log(response)
-            for (var i = 0; i < response.data.length; i++) {
-                showUserDetails(response.data[i])
-            }
+            showUserDetails(response.data)
+            console.log(response)
         })
         .catch((err) => console.log(err))
-    })
-}
-function showUserDetails(obj) {
+    }
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/0a5e5fbcaa49470e9a9394cc1aa41e59/Bookingapp")
+        .then((response) => {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                showUserDetails(response.data[i])
+                }
+        })
+        .catch((err) => console.log(err))
 
-    const ParentEle = document.getElementById('listOfItems');
+})
+    
+function showUserDetails(obj) {
+    let ParentEle = document.getElementById('listOfItems');
     //create child element li
-    const ChildEle = document.createElement('li');
-    //create button as child element
-    const delbtn = document.createElement('button');
-    const editbtn = document.createElement('button');
-    //add class name to button
-    delbtn.className = 'btn';
-    editbtn.className = 'btn';
-    //add text on button
-    var delbtntext = document.createTextNode('Delete');
-    var editbtntext = document.createTextNode('Edit');
-    //add text to button using appendchild
-    delbtn.appendChild(delbtntext);
-    editbtn.appendChild(editbtntext);
+    let ChildEle = document.createElement('li');
+    
     //fetch text content of an element
     ChildEle.textContent = obj.name + '-' + obj.email + '-' + obj.phn;
 
-    //append text content with button
-    ChildEle.appendChild(delbtn);
-    ChildEle.appendChild(editbtn);
-    //append li
-    ParentEle.appendChild(ChildEle);
-    ParentEle.appendChild(ChildEle);
+    
+    let userId = obj._id;
+    // ChildEle.id = userId;
+    
+    //create button as child element
+    let delbtn = document.createElement('button');
+    
+    //add class name to button
+    delbtn.className = 'btn';
+    
+    //add text on button
+    var delbtntext = document.createTextNode('Delete');
+    
+    //add text to button using appendchild
+    delbtn.appendChild(delbtntext);
 
+     //append text content with button
+     ChildEle.appendChild(delbtn);
+     //append li
+     ParentEle.appendChild(ChildEle);
     //for deleting an item
-    // delbtn.onclick = () => {
+    delbtn.onclick = () => {
+        axios.delete(`https://crudcrud.com/api/0a5e5fbcaa49470e9a9394cc1aa41e59/Bookingapp/${userId}`)
+            .then((res) => {
+                showUserDetails(userId)
+                // console.log(res)
+            })
+            .catch((err) => console.log(err))
+        ChildEle = document.getElementById(userId);
+        ParentEle.removeChild(ChildEle);
+       
         
-    //     axios.delete(`https://crudcrud.com/api/58e99c6e52c74033818ad6c2b29a8f7c/bookingApp/${obj._id}`)
-    //     .then((response)=>{
-    //         console.log(obj._id)
-    //         showUserDetails(response)
-    //         console.log(response)
-    //     })
-    //     .catch((err)=>console.log(err))
-    // }
-        
-    //     localStorage.removeItem(obj.email);
-    //     ParentEle.removeChild(ChildEle);
-    // }
+        // localStorage.removeItem(obj.email);
+        // ParentEle.removeChild(ChildEle);
+    }
 
-    //for editing an item
+
+    let editbtn = document.createElement('button');
+    editbtn.className = 'btn';
+    var editbtntext = document.createTextNode('Edit');
+    editbtn.appendChild(editbtntext);
+    ChildEle.appendChild(editbtn);
+    ParentEle.appendChild(ChildEle);
+    // for editing an item
     editbtn.onclick = () => {
         localStorage.removeItem(obj.email);
         ParentEle.removeChild(ChildEle);
@@ -86,6 +95,5 @@ function showUserDetails(obj) {
         document.getElementById('mail').value = obj.email;
         document.getElementById('phn').value = obj.phn;
     }
+
 }
-
-
