55b719bd595c03756b32cc069e572f9eb6f59816
PGaware0310 <pooja.gaware0310>
2023-08-21 19:12:26 +0530

    first

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -33,11 +33,15 @@ const App=() =>{
     },
   ];
   
+  const addExpenseHandler=(expense)=>{
+console.log("In App.js");
+console.log(expense);
+  }
     return (
       <div>
       <h2>Let's get started!</h2>
       {/* <ExpenseForm onSaveExpenseData/> */}
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses items={expenses}/>
        
       </div>
--- my-app/src/components/ExpenseForm/NewExpense.js
+++ my-app/src/components/ExpenseForm/NewExpense.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
-const NewExpense=()=>{
+const NewExpense=(props)=>{
     const saveExpenseDataHandler=(enteredExpenseData)=>{
         const expenseData={
             ...enteredExpenseData,
             id:Math.random().toString()
         };
-        console.log(expenseData);
+        props.onAddExpense(expenseData);
     }
 return(
     <div>
